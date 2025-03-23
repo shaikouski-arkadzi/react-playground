1664303e8afd0389d5b979c04399e6f7006655ae
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-03-23 15:16:36 +0300

    Add stylelint

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
-import { features } from "process";
+import stylelint from "vite-plugin-stylelint";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    stylelint({
+      include: ["src/**/*.{css,scss,less}"],
+      cache: false, // Отключает кеширование
+      throwOnWarning: false, // Не блокирует сборку при предупреждениях
+      throwOnError: false, // Блокирует сборку при ошибках
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Алиас для src
