c087d8e921d8a20e42bf3be8b2aa16d81a338ce9
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2025-03-27 22:10:59 +0300

    Add more features to react-hook-form

--- src/features/react-hook-form/components/ReactHookFormMain.jsx
+++ src/features/react-hook-form/components/ReactHookFormMain.jsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import styles from "./Form.module.css";
 
 function ReactHookFormMain() {
@@ -7,8 +7,15 @@ function ReactHookFormMain() {
     formState: { errors, isValid },
     handleSubmit,
     reset,
+    setValue,
+    watch,
+    control,
   } = useForm({
     mode: "onBlur",
+    defaultValues: {
+      firstName: "test1",
+      age: 18,
+    },
   });
 
   const onSubmit = (data) => {
@@ -17,26 +24,44 @@ function ReactHookFormMain() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-      <input
-        {...register("firstName", {
-          required: "Enter your name",
-          minLength: {
-            value: 5,
-            message: "Length must be more than 5 char",
-          },
-        })}
-        className={`${styles.input} ${
-          errors?.firstName ? styles.inputError : ""
-        }`}
-      />
-      <input type="submit" disabled={!isValid} className={styles.submit} />
-      <div style={{ height: 40 }}>
-        {errors?.firstName && (
-          <p className={styles.errorMessage}>{errors?.firstName.message}</p>
-        )}
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <input
+          {...register("firstName", {
+            required: "Enter your name",
+            minLength: {
+              value: 5,
+              message: "Length must be more than 5 char",
+            },
+          })}
+          className={`${styles.input} ${
+            errors?.firstName ? styles.inputError : ""
+          }`}
+        />
+        <Controller
+          name="age"
+          control={control}
+          render={({ field }) => (
+            <input
+              {...field}
+              className={`${styles.input} ${
+                errors?.firstName ? styles.inputError : ""
+              }`}
+            />
+          )}
+        />
+        <input type="submit" disabled={!isValid} className={styles.submit} />
+        <button type="button" onClick={() => setValue("firstName", "test2")}>
+          Установить значение
+        </button>
+        <div style={{ height: 40 }}>
+          {errors?.firstName && (
+            <p className={styles.errorMessage}>{errors?.firstName.message}</p>
+          )}
+        </div>
+      </form>
+      {watch("firstName")}
+    </>
   );
 }
 
